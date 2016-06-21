be531592ab20c1461859ebbad813eb718e657222
varun sai <varun sai>
2016-06-21 12:15:14 -0500

    checking in all the three programs into workspace02

---
+++ first.java
@@ -0,0 +1,7 @@
+public class First
+{
+  public static void main(String[] args)
+	{
+      System.out.println("This is my first program in java");
+  	}//End of main
+	}//End of FirstJavaProgram Class
