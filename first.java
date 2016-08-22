4a7e897b12d1d5c57b351f1228069ecfb96a0cc3
varunsais <varunsais@gmail.com>
2016-08-22 12:45:07 -0500

    Update first.java

--- first.java
+++ first.java
@@ -4,5 +4,6 @@ class First
 	{
       System.out.println("This is my first program in java");
       System.out.println("Adding the second line");
+      System.out.println("Adding the thrid line");
   	}//End of main
 	}//End of FirstJavaProgram Class
